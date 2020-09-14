660ce79a68a8aba2cbaa2eb687321cabb72b5153
Mr Speaker <mrspeaker@gmail.com>
2020-09-14 11:07:24 -0400

    Lots o sprites

--- worms.asm
+++ worms.asm
@@ -22,8 +22,7 @@ init_irq:
                 lda $D011               // clear high bit or raster line
                 and #%01111111
                 sta $D011
-
-                lda #49
+                lda spr_lines
                 sta $D012
 
                 lda #<irq
@@ -33,20 +32,27 @@ init_irq:
                 rts
 
 load_sprites:
-                lda #%00000011
+                lda #%00011111
                 sta $D015
 
                 lda #$ff
                 sta $D000               // pos
                 sta $D002
-                lda #$60
+                sta $D004
+                sta $D006
+                sta $D008
+                lda #$50
                 sta $D001
                 sta $D003
+                sta $D005
+                sta $D007
+                sta $D009
 
-                lda #%00000010
+                lda #%00000110
                 sta $D01C               // multicolor
                 lda #0
                 sta $D027               // spr0 color
+                sta $D029
                 lda #4
                 sta $D028               // spr1 color
                 lda #13
@@ -56,8 +62,11 @@ load_sprites:
 
                 lda #$340/64            // mem pointers
                 sta $7F8
+                sta $7fb
+                sta $7fc
                 lda #$340/64+1
                 sta $7f9
+                sta $7fa
 
                 ldx #64*2               // load 2 sprites
 _load:
@@ -72,14 +81,31 @@ irq:
                 inc $D020
                 ldx spr_last
                 lda spr_lines,x
+                // TODO: wait until new D012 then position.
+                // would fix jitter
+                adc #2 // allow a line! Else, doesn't show
                 sta $D001
-                inc $D001
                 sta $D003
-                inc $D003
+                sta $D005
+                sta $D007
+                sta $D009
                 lda spr_off,x
                 adc #180
                 sta $D000
                 sta $D002
+                adc #25
+                adc spr_off,x
+                sta $D004
+                lda $D006
+                clc
+                adc spr_off,x
+                asl
+                sta $D006
+
+                stx $D025
+                inc $D025
+
+                inc spr_off,x
 
                 inc spr_last
                 lda spr_last
@@ -109,7 +135,7 @@ _irq_d:
                 rti
 
 spr_last:       .byte 1
-spr_lines:      .byte 49,72,94,116,138,160,182,204,226
+spr_lines:      .byte 50,72,94,116,138,160,182,204,226
 spr_off:        .byte 0,-15,-24,-15,0,15,24,15,0
 sprites:
                 .import binary "worm.bin"
