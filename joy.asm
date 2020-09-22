a6582b83361e1bc6550c4449390b13ae404da359
Mr Speaker <mrspeaker@gmail.com>
2020-09-21 22:02:21 -0400

    joy

--- joy.asm
+++ joy.asm
@@ -11,12 +11,50 @@ loop:
                 jmp loop
 
 init:
+                sei
                 lda #$0
                 sta $d020
                 sta $d021
 
                 jsr init_sprites
+
+
+                lda #%01111111
+                sta $dc0d               // Turn off CIAs Timer interrupts
+                sta $dd0d
+                lda $dc0d               // cancel all CIA-IRQs in queue/unprocessed
+                lda $dd0d
+
+                lda #%11110100
+                sta $d01a
+                sta $d019
+
+                lda #<irq
+                ldx #>irq
+                sta $314
+                stx $315
+
+                cli
+
                 rts
+irq:
+                dec $d020
+
+                lda $d01e
+                and #%00000001
+                beq !+
+
+                lda $dc04
+                eor $dc05
+                sta $d003
+!:
+                asl $d019
+                pla
+                tay
+                pla
+                tax
+                pla
+                rti
 
 init_sprites:
                 lda #%00000011
@@ -35,9 +73,11 @@ init_sprites:
                 sta $7f8
                 sta $7f9
 
-                lda #180
+                lda #70
                 sta $d002
                 sta $d003
+                lda #20
+                sta $d000
 
                 rts
 
@@ -48,17 +88,13 @@ update:
                 clc
                 adc player_dir
                 bcc !+
-                // why does this work without resseting?
                 tax
                 lda $d010
                 eor #$1
                 sta $d010
                 txa
-
 !:
                 sta player_x
-
-
                 sta $d000
                 stx $d001
 
@@ -72,19 +108,10 @@ update:
                 inc player_y
 !:
 
-                lda $d01e
-                and #%00000001
-                beq !+
-                dec $d020
-                lda $dc04
-                eor $dc05
-                sta $d003
-!:
-
                 rts
 
-player_x:       .byte 80
+player_x:       .byte 20
 player_y:       .byte 60
 player_dir:     .byte 1
 spr_bg:
-                .fill 64, $aa
+                .fill 64, [$aa, $55]
