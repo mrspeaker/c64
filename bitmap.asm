ca936aa7736367227ef71b4d9e23e509b9daa7e6
Mr Speaker <mrspeaker@gmail.com>
2020-09-23 08:41:08 -0400

    smooth scrollz

--- bitmap.asm
+++ bitmap.asm
@@ -42,29 +42,44 @@ _sprite_init:
 
                 // nah, dunno why sprites work like this
                 // (it disappears out of bitmap)
-                lda #$2000/64
+                lda #$340/64
                 sta $7F8
                 sta $7f9
+
+                ldx #$40
+!:
+                lda spr,x
+                sta $340,x
+                dex
+                bpl !-
 
 _write_message:
                 ldx #0
 !:
                 lda msg,x
                 beq _loop
-                sta $400+40*13+11,x
+                sta $400+40*14,x
+                txa
+                and #%00000111
+                tay
+                lda cols,y
+                sta $d800+40*14,x
                 inx
                 jmp !-
 _loop:
+                lda #0 // hide the edges
+                sta $d800+40*14
+                sta $d800+40*15-1
+
                 jmp *
 
 irq:
                 dec $d019
 
-
                 inc $d001
                 dec $d003
 
-                lda #$8f
+                lda #$91
                 sta $d012
                 lda #<irq2
                 ldx #>irq2
@@ -87,33 +102,30 @@ irq:
                 rti
 
 irq2:
-                dec $d019
+                lda $d012
+                cmp #$92
+                bmi irq2
 
                 // text mode
                 lda #%00011011
                 sta $d011
                 lda #%00010101
                 sta $d018
-                // lda #%11001000
-                // sta $d016
-
-                lda $D016
-                and #%11111000
-                ora xoffi
-                sta $D016
+                lda #%11001000
+                sta $d016
 
                 dec xoffi
                 bpl !+
                 ldx #7
                 stx xoffi
 
-                lda $400+40*13
-                sta $3ff+40*14
+                lda $400+40*14
+                sta $3ff+40*15
 
                 ldy #0
 !b:
-                lda $401+40*13,y
-                sta $400+40*13,y
+                lda $401+40*14,y
+                sta $400+40*14,y
                 iny
                 cpy #39
                 bne !b-
@@ -132,7 +144,7 @@ irq2:
                 sta $314
                 stx $315
 
-
+                dec $d019
                 pla
                 tay
                 pla
@@ -141,9 +153,11 @@ irq2:
 
                 rti
 
-msg:            .text "mr speaker rulez"
+msg:            .text "            mr speaker rulez            "
                 .byte 0
 xoffi:          .byte 7
+cols:           .byte 11,12,15,3,3,15,12,11
+spr:            .fill 64, $aa
 
  * = $0c00 "ScreenRam_1"; screenRam_1: .fill picture1.getScreenRamSize(), picture1.getScreenRam(i)
  * = $1c00 "ColorRam_1"; colorRam_1: .fill picture1.getColorRamSize(), picture1.getColorRam(i)
