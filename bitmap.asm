044eb0f10b17026bc45e152f945e5fcbd906f248
Mr Speaker <mrspeaker@gmail.com>
2020-09-25 11:05:11 -0400

    Update row

--- bitmap.asm
+++ bitmap.asm
@@ -2,6 +2,8 @@
 
             BasicUpstart2(entry)
 
+            .const SCR_ROW = 18
+
 entry:
                 lda #0
                 sta $d021
@@ -9,7 +11,7 @@ entry:
 
                 sei
 
-                lda #%01111111 // disable CIA timer
+                lda #%01111111          // disable CIA timer
                 sta $dc0d
                 lda $dc0d
 
@@ -29,64 +31,37 @@ entry:
                 stx $315
 
                 cli
-
-_sprite_init:
-                lda #$3
-                sta $d015
-
-                lda #$b0
-                sta $d000
-                sta $d001
-                sta $d002
-                sta $d003
-
-                // nah, dunno why sprites work like this
-                // (it disappears out of bitmap)
-                lda #$340/64
-                sta $7F8
-                sta $7f9
-
-                ldx #$40
-!:
-                lda spr,x
-                sta $340,x
-                dex
-                bpl !-
-
 _write_message:
                 ldx #0
 !:
                 lda msg,x
                 beq _loop
-                sta $400+40*14,x
+                sta $400+40*SCR_ROW,x
                 txa
                 and #%00000111
                 tay
                 lda cols,y
-                sta $d800+40*14,x
+                sta $d800+40*SCR_ROW,x
                 inx
                 jmp !-
 _loop:
                 lda #0 // hide the edges
-                sta $d800+40*14
-                sta $d800+40*15-1
+                sta $d800+40*SCR_ROW
+                sta $d800+40*(SCR_ROW+1)-1
 
                 jmp *
 
 irq:
                 dec $d019
 
-                inc $d001
-                dec $d003
-
-                lda #$91
+                lda #$87
                 sta $d012
                 lda #<irq2
                 ldx #>irq2
                 sta $314
                 stx $315
 
-                lda #%00111011
+                lda #%00111000
                 sta $d011
                 lda #%00111000
                 sta $d018
@@ -103,11 +78,12 @@ irq:
 
 irq2:
                 lda $d012
-                cmp #$92
+                cmp #$85
                 bmi irq2
 
+
                 // text mode
-                lda #%00011011
+                lda #%00011000
                 sta $d011
                 lda #%00010101
                 sta $d018
@@ -119,13 +95,13 @@ irq2:
                 ldx #7
                 stx xoffi
 
-                lda $400+40*14
-                sta $3ff+40*15
+                lda $400+40*SCR_ROW
+                sta $3ff+40*(SCR_ROW+1)
 
                 ldy #0
 !b:
-                lda $401+40*14,y
-                sta $400+40*14,y
+                lda $401+40*SCR_ROW,y
+                sta $400+40*SCR_ROW,y
                 iny
                 cpy #39
                 bne !b-
@@ -153,7 +129,8 @@ irq2:
 
                 rti
 
-msg:            .text "            mr speaker rulez            "
+msg:
+                .text " mr speaker rulez, scrollin' like 1984."
                 .byte 0
 xoffi:          .byte 7
 cols:           .byte 11,12,15,3,3,15,12,11
